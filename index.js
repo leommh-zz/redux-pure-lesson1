942e8a1a5be4359badbaaed55976f975ca495151
Leonardo Morini <leo.morini.nn@gmail.com>
2018-10-31 20:09:57 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+function createStore(){
+
+    // The store 
+    // 1. The state
+    // 2. Get the state
+    // 3. Listen to changes on the state
+    // 4. Update the state 
+
+    let state
+
+    const getState = () => state
+
+    return {
+        getState
+    }
+
+}
+
+
